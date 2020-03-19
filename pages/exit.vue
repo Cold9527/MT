c889ecf2863dcfcae032923a4ff0aaf241f495e8
Cold9527 <343707313@qq.com>
2020-03-19 10:32:59 +0800

    注册登录退出功能完成

---
+++ pages/exit.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+export default {
+   name:'blank',
+   middleware:async (ctx)=>{
+       let {status, data} = await ctx.$axios.get('/users/exit')
+       if(status === 200&&data&&data.code === 0){
+           window.location.href = '/'
+       }
+   }
+
+}
+</script>
+
+<style>
+
+</style>
